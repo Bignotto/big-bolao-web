d6d9094945f1cf964b85553744d1f1951c922d3d
Thiago Bignotto <bignotto@gmail.com>
2022-10-26 18:10:16 -0300

    better confirm email page

--- pages/confirm/index.tsx
+++ pages/confirm/index.tsx
@@ -1,62 +1,31 @@
-import { useEffect, useState } from "react";
 import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
-import { supabase } from "../../utils/supabase";
-
 const Confirm: NextPage = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [allRight, setAllRight] = useState(false);
-  const [newEmail, setNewEmail] = useState("");
-
-  async function loadUser() {
-    const session = await supabase.auth.getSession();
-
-    console.log({ session });
-    if (!session.data.session) {
-      setAllRight(false);
-      setIsLoading(false);
-      return;
-    }
-
-    const theUser = await supabase.auth.getUser();
-
-    setNewEmail(theUser.data.user?.email ?? "erro!");
-    setIsLoading(false);
-  }
-
-  useEffect(() => {
-    loadUser();
-  }, []);
-
   return (
     <div className="flex min-h-screen flex-col  justify-center py-2 bg-zinc-900">
       <Head>
         <title>Bolão da Copa</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {isLoading ? (
-        <h1 className="text-white text-lg font-bold font-Kanit text-center mt-5">
-          Recuperando informações...
-        </h1>
-      ) : (
-        <main className="flex flex-col items-center py-5">
-          <Image
-            src="/logo.png"
-            alt="Bolão da Copa logo"
-            width={474}
-            height={263}
-          />
 
-          <h1 className="text-white text-lg font-bold font-Kanit text-center mt-5">
-            Seu endereço de e-mail foi confirmado!
-          </h1>
-          <p>
-            Use este novo endereço para fazer login no aplicativo Bolão da Copa.
-          </p>
-        </main>
-      )}
+      <main className="flex flex-col items-center py-5">
+        <Image
+          src="/logo.png"
+          alt="Bolão da Copa logo"
+          width={474}
+          height={263}
+        />
+
+        <h1 className="text-white text-lg font-bold font-Kanit text-center mt-5">
+          Seu endereço de e-mail foi confirmado!
+        </h1>
+        <p className="text-white font font-Kanit text-sm">
+          Use este novo endereço para fazer login no aplicativo Bolão da Copa.
+        </p>
+      </main>
+
       <footer className="flex justify-center">
         <p className="text-white font-Kanit text-xs">
           Thiago Bignotto 2022 © bignotto@gmail.com
